76c529f2678868695097db8bffdb03cc5d1dcc09
Jatin Mehra <72027234+jatin22-exe@users.noreply.github.com>
2020-09-30 10:40:25 -0700

    Update fact using recursion.py

--- fact using recursion.py
+++ fact using recursion.py
@@ -1,13 +1,17 @@
-def fibo(n):
-     if n<=1:
-          return n
-     else:
-          fibo(n-1)+fibo(n-2)
-num=int(input("enter the number"))
-if num<0:
-     print("factorial of negative number is ")
-if num==0:
-     print("1")
-if num>0:
-     print(fibo(num))
-     
+# Factorial of a number using recursion
+
+def recur_factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*recur_factorial(n-1)
+
+num = 7
+
+# check if the number is negative
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print("The factorial of", num, "is", recur_factorial(num))
